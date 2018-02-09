f3040e6adbc3e269304ea1ad8499c660a967aa5b
nikhil@timebyping.com <nikhilsgahlot@gmail.com>
2018-02-08 19:17:26 -0800

    Initial Commit

---
+++ UCBMUNXXII/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  UCBMUNXXII
+//
+//  Created by Nikhil Gahlot on 2/8/18.
+//  Copyright © 2018 UCBMUN. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
